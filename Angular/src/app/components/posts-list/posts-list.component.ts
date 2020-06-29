7c38be87f9a69154b86cf80709678af4608d7ada
lisandrof <lxweb102@gmail.com>
2020-06-29 18:10:47 -0300

    Adding a real connection no HTTPSserver

--- Angular/src/app/components/posts-list/posts-list.component.ts
+++ Angular/src/app/components/posts-list/posts-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/common/post';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-posts-list',
@@ -8,30 +9,15 @@ import { Post } from 'src/app/common/post';
 })
 export class PostsListComponent implements OnInit {
 
-  posts = [
-    {
-      "userId": 1,
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      "userId": 1,
-      "id": 2,
-      "title": "qui est esse",
-      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-      "userId": 1,
-      "id": 3,
-      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    }
-  ];
+  posts:Post[];
 
-  constructor() { }
+  constructor(private postService:PostsService) { }
 
   ngOnInit(): void {
+    this.postService.getAllPost().subscribe( 
+      (data) => {
+        this.posts = <Post[]>data;
+      } );
   }
 
   mostrarId(id:number){
@@ -39,6 +25,7 @@ export class PostsListComponent implements OnInit {
 
   }
 
+  
   createNewPost(event:Post){
     let newPost:Post = event;
     this.posts.push(newPost);
