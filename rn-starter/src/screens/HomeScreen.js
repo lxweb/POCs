9af451f484d4c5c4058b1ec82f4569df7b8c2635
lisandrof <lxweb102@gmail.com>
2020-08-04 20:26:37 -0300

    Adding new screens with styles

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+
+  let secondLineText = "Home Second Line";
+  return <View>
+    <Text style={styles.text}>
+      HomeScreen
+    </Text>
+    <Text style={styles.subHeaderStyle}>
+      {secondLineText}
+    </Text>
+  </View>;
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
+  },
+  subHeaderStyle:{
+    fontSize: 20
   }
 });
 
